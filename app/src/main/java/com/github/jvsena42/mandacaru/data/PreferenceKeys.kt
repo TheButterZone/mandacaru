a4f8831aaa32d0b8b011cdc621c87561186b30ba
Alwin <thebutterzone@cox.net>
2026-07-07 12:43:15 +0000

    Update PreferenceKeys.kt

--- app/src/main/java/com/github/jvsena42/mandacaru/data/PreferenceKeys.kt
+++ app/src/main/java/com/github/jvsena42/mandacaru/data/PreferenceKeys.kt
@@ -4,6 +4,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 enum class PreferenceKeys(val dataStoreKey: Preferences.Key<String>) {
+    FLORESTA_DIRECTORY(stringPreferencesKey("FLORESTA_DIRECTORY")),
     CURRENT_NETWORK(stringPreferencesKey("CURRENT_NETWORK")),
     CURRENT_RPC_PORT(stringPreferencesKey("CURRENT_RPC_PORT")),
     PENDING_UTREEXO_SNAPSHOT(stringPreferencesKey("PENDING_UTREEXO_SNAPSHOT")),
