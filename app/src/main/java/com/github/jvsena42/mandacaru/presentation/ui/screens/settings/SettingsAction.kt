516fc8fe034e64eb4032f748e1eed0f4cdd9cdbd
Alwin <thebutterzone@cox.net>
2026-07-07 19:58:40 +0000

    Update SettingsAction.kt

--- app/src/main/java/com/github/jvsena42/mandacaru/presentation/ui/screens/settings/SettingsAction.kt
+++ app/src/main/java/com/github/jvsena42/mandacaru/presentation/ui/screens/settings/SettingsAction.kt
@@ -1,39 +1,55 @@
 package com.github.jvsena42.mandacaru.presentation.ui.screens.settings
 
-import com.florestad.Network
+enum class FlorestaStorageLocation {
+    INTERNAL,
+    EXTERNAL,
+}
 
 sealed interface SettingsAction {
-    data class OnDescriptorChanged(val descriptor: String): SettingsAction
-    data class OnNodeAddressChanged(val address: String): SettingsAction
-    data class OnNetworkSelected(val network: String): SettingsAction
-    object OnClickUpdateDescriptor: SettingsAction
-    object OnClickScanDescriptor: SettingsAction
-    object OnDismissDescriptorScanner: SettingsAction
-    data class OnDescriptorQrFrameScanned(val raw: String): SettingsAction
-    object OnConfirmScannedDescriptor: SettingsAction
-    object OnDismissScannedDescriptor: SettingsAction
-    data class OnDescriptorCopied(val descriptor: String): SettingsAction
-    object OnClickConnectNode: SettingsAction
-    object OnClickRescan: SettingsAction
-    object ClearSnackBarMessage: SettingsAction
-    object ToggleDescriptorsExpanded: SettingsAction
-    object ToggleNetworkExpanded: SettingsAction
-    object ToggleNodeExpanded: SettingsAction
-    object ToggleAboutExpanded: SettingsAction
-    object ToggleDonateExpanded: SettingsAction
-    object OnClickExportLogs: SettingsAction
-    object OnClickGetUpdate: SettingsAction
-    object OnClickCheckForUpdates: SettingsAction
-    object ToggleBirthdayExpanded: SettingsAction
-    object OnClickChangeBirthdayYear: SettingsAction
-    data class OnBirthdayYearSelected(val year: Int): SettingsAction
-    object OnDismissBirthdayPicker: SettingsAction
-    object OnConfirmBirthdayRestart: SettingsAction
-    object OnCancelBirthdayRestart: SettingsAction
-    object ToggleDataUsageExpanded: SettingsAction
-    data class OnToggleMobileData(val enabled: Boolean): SettingsAction
-    data class OnToggleAdvancedFeatures(val enabled: Boolean): SettingsAction
-    data class OnFlorestaDirectorySelected(val path: String): SettingsAction
-    object ToggleDeveloperToolsExpanded: SettingsAction
-    object OnClickViewLogs: SettingsAction
+    data class OnDescriptorChanged(val descriptor: String) : SettingsAction
+    data class OnNodeAddressChanged(val address: String) : SettingsAction
+    data class OnNetworkSelected(val network: String) : SettingsAction
+
+    object OnClickUpdateDescriptor : SettingsAction
+    object OnClickScanDescriptor : SettingsAction
+    object OnDismissDescriptorScanner : SettingsAction
+    data class OnDescriptorQrFrameScanned(val raw: String) : SettingsAction
+    object OnConfirmScannedDescriptor : SettingsAction
+    object OnDismissScannedDescriptor : SettingsAction
+    data class OnDescriptorCopied(val descriptor: String) : SettingsAction
+
+    object OnClickConnectNode : SettingsAction
+    object OnClickRescan : SettingsAction
+
+    object ClearSnackBarMessage : SettingsAction
+
+    object ToggleDescriptorsExpanded : SettingsAction
+    object ToggleNetworkExpanded : SettingsAction
+    object ToggleNodeExpanded : SettingsAction
+    object ToggleAboutExpanded : SettingsAction
+    object ToggleDonateExpanded : SettingsAction
+
+    object OnClickExportLogs : SettingsAction
+
+    object OnClickGetUpdate : SettingsAction
+    object OnClickCheckForUpdates : SettingsAction
+
+    object ToggleBirthdayExpanded : SettingsAction
+    object OnClickChangeBirthdayYear : SettingsAction
+    data class OnBirthdayYearSelected(val year: Int) : SettingsAction
+    object OnDismissBirthdayPicker : SettingsAction
+    object OnConfirmBirthdayRestart : SettingsAction
+    object OnCancelBirthdayRestart : SettingsAction
+
+    object ToggleDataUsageExpanded : SettingsAction
+    data class OnToggleMobileData(val enabled: Boolean) : SettingsAction
+
+    data class OnToggleAdvancedFeatures(val enabled: Boolean) : SettingsAction
+
+    data class OnStorageLocationSelected(
+        val location: FlorestaStorageLocation
+    ) : SettingsAction
+
+    object ToggleDeveloperToolsExpanded : SettingsAction
+    object OnClickViewLogs : SettingsAction
 }
